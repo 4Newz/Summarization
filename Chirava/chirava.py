dd4b9f6139c1c0e01472815e6f9d6e1d3d2b42ff
marahim20 <minhajabdulraheem@gmail.com>
2024-01-30 11:58:21 +0530

    feat(scraper): endpoint expose

---
+++ Chirava/chirava.py
@@ -0,0 +1,20 @@
+from newspaper import Article
+
+class Scraper:
+    def __init__(self, url: str):
+        self.url = url
+        self.article = Article(url)
+        self.article.download()
+        self.article.parse()
+        self.article.nlp()
+    async def runner(self):
+        response = {
+            "title": self.article.title,
+            "text": self.article.text,
+            "keywords": self.article.keywords,
+            "summary": self.article.summary,
+            "authors": self.article.authors,
+            "publish_date": self.article.publish_date,
+            "top_image": self.article.top_image,
+        }
+        return response
